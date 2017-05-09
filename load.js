92a139dbadeff6bf2ff6eec7b4bc455f0edd22a7
Gavin Berkowitz <gberkowitz@logmein.com>
2017-05-09 19:02:34 -0400

    Getting close - still not linking correctly to orgs

--- load.js
+++ load.js
@@ -1,11 +1,11 @@
 var request = require('request');
-var manage = request.defaults({baseUrl: "https://blueprint.xively.com:443/api/manage/"});
+var manage = request.defaults({baseUrl: "https://blueprint.xively.com:443/api/v1/", json:true});
 var config = require('./config.js');
 var accountId = config.accountId;
 var credentials = require('./credentials.js');
 var faker = require('faker/locale/en_US'); //For user data
 var groups = []; //This will probably need to be re-thought
-var JWT;
+var authToken;
 
 /*
     Process idea: create orgs first and save ids. Use ratio of orgs:devices:users to associate while creating
@@ -13,18 +13,21 @@ var JWT;
     to do nested orgs, but not much I think. Probably only works for ratio > 1.
 */
 
-var createXivelyObject = function(objectConfig,createBodyFn,processResponseFn){
-    for(var upTo in config.asManyAsINeed){
+var createXivelyObject = function(objectConfig,createBodyFn,processResponseFn,callback){
+    var upTo = 0;
+    while(++upTo <= objectConfig.asManyAsINeed){ // <-- Pretty sure I heard that prepended incrementors are the sign of a maniac
         var body = createBodyFn(objectConfig, upTo);
-        manage.post(config.postURL,function(req,res){
-            if(typeof processResponseFn === "function") processResponseFn(res);
+        manage.post({url:objectConfig.postURL,headers:{'authorization': authToken}, body:body}, function(err, data){
+            console.log(data.body);
+            if(typeof processResponseFn === "function") processResponseFn(data.body);
         });
     }
+    callback(); //oy
 }
 
 
 /* Body Generators */
-var createUserBody = function(userConfig){
+var createUserBody = function(userConfig,upTo){
     return {
         "createIdmUser":userConfig.createIdmUser, //Not parsing for bad data atm
         "idmUserEmail": faker.internet.exampleEmail(),
@@ -39,7 +42,6 @@ var createUserBody = function(userConfig){
 }
 
 var createDeviceBody = function(deviceConfig,upTo){
-
     return {
         "accountId":accountId,
         "organizationId": linkToGroupSimple(upTo),
@@ -62,7 +64,8 @@ var createGroupBody = function(groupConfig, upTo){
 /* Response Processors */
 var processGroupResponse = function(groupResponse){
     //Do some error processing
-    groups.push(groupResponse.body.id);
+    console.log(groupResponse.organization.id);
+    groups.push(groupResponse.organization.id);
 }
 
 /* Additional Helper Functions */
@@ -70,35 +73,40 @@ var createIncrementingName = function(namePrefix, num){
     return namePrefix + num;
 }
 
-var doLogin = function(username,password){
+var doLogin = function(username,password, callback){
     var body = {
         "emailAddress":credentials.username,
         "password":credentials.password,
         "accountId":accountId
     }
     request.post({url:"https://id.xively.com/api/v1/auth/login-user",body:body, json:true},function(err,data){
-        console.log(err);
-        console.log(data.body);
-        manage.defaults({headers: {'Authorization':"Bearer " +  res.body.jwt}}); 
+        authToken = "Bearer " + data.body.jwt;
+        callback();
     });
 }
 
 var linkToGroupSimple = function(upTo){
+    console.log(groups);
     return groups[upTo-1];
 }
 
 /* Actual Process */
 var run = function(){
-    doLogin(credentials.username,credentials.password); //It's been too long, can I count on this to be synchronous?
-    //createXivelyObject(config.groups,createGroupBody,processGroupResponse);
-    //createXivelyObject(config.devices,createDeviceBody);
-    //createXivelyObject(config.users,createUserBody);
+    doLogin(credentials.username,credentials.password,function(){ //There's GOT to be a better way!
+        createXivelyObject(config.groups,createGroupBody,processGroupResponse,function(){
+            //createXivelyObject(config.devices,createDeviceBody,null,function(){
+              //  createXivelyObject(config.users,createUserBody,null,function(){}); //yes I know
+           // });
+        });
+    }); 
 }
 
 run();
 
 
 /* TODO List */
+//P0: What's the preferred way to not have stupid callbacks?
+//P1: Delete all data from an account. Keep this in a separate script.
 //P1: Yell and scream if number of groups is < number of users or devices (until this means something)
 //P1: Yell and scream if number of devices != number of users (until this means something)
 //P1: Report errors
