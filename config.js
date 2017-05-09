baa4c1e555ed65817314f1364f6fe2e093867528
Gavin Berkowitz <gberkowitz@logmein.com>
2017-05-09 17:08:27 -0400

    Create body functions

--- config.js
+++ config.js
@@ -3,16 +3,20 @@ module.exports = {
     "devices":{
         "templateId":"768e7c53-614a-457b-8f58-2efea45254d7",
         "asManyAsINeed": 1,
-        "postURL":"devices"
+        "postURL":"devices",
+        "namePrefix": "Device Serial - "
     },
     "groups":{
         "templateId":"c5f77aa9-3f5d-4fff-901d-fef8f66deec0",
         "asManyAsINeed": 1,
-        "postURL":"groups"
+        "postURL":"organizations",
+        "namePrefix": "Group - "
     },
     "users":{
         "templateId":"0fc34c26-1a2c-4164-a94d-b71471e1fb09",
         "asManyAsINeed": 1,
-        "postURL":"end-users"
+        "postURL":"end-users",
+        "namePrefix": "",
+        "createIdmUser":true
     }
 }
--- load.js
+++ load.js
@@ -1,23 +1,89 @@
 var request = require('request');
 var manage = request.defaults({baseUrl: "https://blueprint.xively.com:443/api/manage/"});
 var config = require('./config.js');
-
-//TODO p4: Batch?
-
-//TODO Ok, now that you've started, do you want to configure your environment or create some data?
-
-//TODO p3: var goConfigure = function(){};
+var accountId = config.accountId;
+var credentials = require('./credentials.js');
+var faker = require('faker/locale/en_US'); //For user data
+var groups = []; //This will probably need to be re-thought
+var JWT;
 
 /*
     Process idea: create orgs first and save ids. Use ratio of orgs:devices:users to associate while creating
     devices + users. I believe when upTo % ratio = 0, pop the next orgId off the array. Will become more complex
-    to do nested orgs, but not much I think.
+    to do nested orgs, but not much I think. Probably only works for ratio > 1.
 */
 
-var createXivelyObject = function(config,bodyFn){
+var createXivelyObject = function(accountId,objectConfig,createBodyFn,processResponseFn){
     for(var upTo in config.asManyAsINeed){
-        var body = bodyFn();
-        manage.post(function(req,res){
+        var body = createBodyFn(objectConfig, upTo);
+        manage.post(config.postURL,function(req,res){
+            if(typeof processResponseFn === "function") processResponseFn(res);
         });
     }
 }
+
+
+/* Body Generators */
+var createUserBody = function(userConfig){
+    return {
+        "createIdmUser":userConfig.createIdmUser, //Not parsing for bad data atm
+        "idmUserEmail": faker.internet.exampleEmail(),
+        "idmUserProfile": {
+            "firstName": faker.name.firstName(),
+            "lastName": faker.name.lastName()
+        },
+        "accountId": accountId,
+        "endUserTemplateId": userConfig.templateId,
+        "organizationId": linkToGroup(upTo)
+    }
+}
+
+var createDeviceBody = function(deviceConfig,upTo){
+
+    return {
+        "accountId":accountId,
+        "organizationId": linkToGroup(upTo),
+        "serialNumber": createIncrementingName(deviceConfig.namePrefix),
+        "deviceTemplateId": deviceConfig.templateId,
+        "latitude": faker.address.latitude(),
+        "longitude": faker.address.longitude(),
+    }
+}
+
+var createGroupBody = function(groupConfig, upTo){
+    return {
+        "accountId":accountId,
+        "name": createIncrementingName(groupConfig.namePrefix,upTo),
+        "organizationTemplateId": groupConfig.templateId
+    }
+}
+
+
+/* Response Processors */
+var processGroupResponse = function(groupResponse){
+
+}
+
+/* Additional Helper Functions */
+var createIncrementingName = function(namePrefix, num){
+    return namePrefix + num;
+}
+
+var login = function(username,password){
+    
+}
+
+
+/* TODO List */
+//P0: Log in
+//P1: Yell and scream if number of groups is < number of users or devices (until this means something)
+//P1: Yell and scream if number of devices != number of users (until this means something)
+//P1: Report errors
+//P2: Create devices and attach to existing groups. Confirm with user that numDevices should be attached to numGroups (especially if numGroups < numDevices)
+//P2: Allow script to configure your environment OR create some data
+//P2: Only create devices, or groups, or users (and attach them to existing)
+//P3: Batch
+//P3: async/await
+//P4: Progress Bar
+//P4: Typescript
+//P4: Am I doing anything the "old way", node-wise?
